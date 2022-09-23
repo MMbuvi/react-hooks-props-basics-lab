b9f78d32cb1b989e36792ab8da4ef36b6490a441
Meshack <mbuvi.meshack99@gmail.com>
2022-09-23 20:34:08 +0300

    Completed assignment

---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Links (props){
+    return(
+    <div>
+    <h3 id="Links">Links
+    <a href={props.github}>{props.github}</a>
+    <a href={props.linkedin}>{props.linkedin}</a>
+    </h3>
+    </div>
+    )
+}
+
+export default Links;
